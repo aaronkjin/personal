016cac6cb4901016b138ffbd97be0c81576ba42f
Aaron Jin <58490258+aaronkjin@users.noreply.github.com>
2023-08-11 03:29:17 -0700

    Add files via upload

---
+++ components/Footer/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="absolute bottom-0 left-0 right-0 flex justify-center">
+      <h1 className="text-xs text-bold p-2 laptop:p-0">
+        &#169; Aaron Jin. All rights reserved.
+      </h1>
+    </div>
+  );
+};
+
+export default Footer;
---
+++ components/Header/index.js
@@ -0,0 +1,40 @@
+import React, { useEffect, useState } from "react";
+import { useTheme } from "next-themes";
+import { useRouter } from "next/router";
+import Button from "../Button";
+import { FiSun, FiMoon } from "react-icons/fi";
+
+const Header = () => {
+  const { theme, setTheme } = useTheme();
+  const [mounted, setMounted] = useState(false);
+
+  // Use the router only on the client-side
+  const router = useRouter();
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  return (
+    <div className={`header-container ${theme === "dark" ? "dark-bg" : ""}`}>
+      <div className="header-content">
+        <div className="flex items-center justify-between p-2">
+          <div className="flex ml-auto">
+            <Button onClick={() => router.push("/")}>Home</Button>
+            <Button onClick={() => router.push("/about")}>About</Button>
+            <Button onClick={() => router.push("/projects")}>Projects</Button>
+            {mounted && theme && (
+              <Button
+                onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
+              >
+                {theme === "dark" ? <FiMoon size={20} /> : <FiSun size={20} />}
+              </Button>
+            )}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
