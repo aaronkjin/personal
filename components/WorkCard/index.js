79d84e5b951c8ae61d69fa1ee58a508d95b1dacb
Aaron Jin <58490258+aaronkjin@users.noreply.github.com>
2023-08-16 20:55:55 -0700

    Made some things clickable for each project

--- components/WorkCard/index.js
+++ components/WorkCard/index.js
@@ -3,12 +3,12 @@ import Image from "next/image";
 
 const WorkCard = ({ img, name, description, onClick }) => {
   return (
-    <div
-      className="rounded-lg p-2 first:ml-0 link laptop:w-[85%]"
-      onClick={onClick}
-    >
+    <div className="rounded-lg p-2 first:ml-0 link laptop:w-[85%]">
       <div className="flex flex-col laptop:mt-5">
-        <div className="relative rounded-lg transition-all ease-out duration-300 h-48 mob:h-auto">
+        <div
+          className="relative rounded-lg transition-all ease-out duration-300 h-48 mob:h-auto cursor-pointer"
+          onClick={onClick}
+        >
           <Image
             alt={name}
             className="h-full w-full object-cover rounded-lg hover:scale-110 transition-all ease-out duration-300"
