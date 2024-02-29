56a426f1c4cc418e789caeb7b071bb5d66a34355
aaronkjin <58490258+hoxykidmilli@users.noreply.github.com>
2024-02-29 15:18:23 -0800

    footer

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <div className="absolute bottom-0 left-0 right-0 flex justify-center">
       <h1 className="text-xs text-bold p-2 laptop:p-0">
-        Built by Aaron Jin.
+        Built from scratch by Aaron Jin.
         {/* &#169; Aaron Jin. All rights reserved. */}
       </h1>
     </div>
