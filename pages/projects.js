5a7a8294e47d9972ab87e47709c91e65f25825c5
aaronkjin <58490258+hoxykidmilli@users.noreply.github.com>
2024-05-17 21:16:43 -0700

    subsections for projects

--- pages/projects.js
+++ pages/projects.js
@@ -1,9 +1,12 @@
 import Head from "next/head";
-import WorkCard from "../components/WorkCard";
+import { useTheme } from "next-themes";
 
+import WorkCard from "../components/WorkCard";
 import Layout from "../components/Layout";
 
 export default function Projects() {
+  const { theme } = useTheme();
+
   return (
     <Layout footerType="normal">
       <Head>
@@ -22,129 +25,145 @@ export default function Projects() {
         </div>
       </div>
 
-      <div className="mt-6 tablet:mt-10 laptop:mt-15 p-2 tablet:p-0">
-        <div className="mt-6 tablet:mt-10 laptop:mt-15 pb-16 grid grid-cols-1 tablet:grid-cols-2">
-          {/* Mentore */}
-          <div>
-            <WorkCard
-              key="Mentore"
-              img="/images/mentore.png"
-              name="Mentore"
-              description="An LLM-integrated mentor-matching platform. Built with JavaScript, React, HTML/CSS, OpenAI, Firebase, Flask, and Python's various ML libraries."
-              onClick={() =>
-                window.open("https://github.com/aaronkjin/mentore")
-              }
-            />
-          </div>
+      {/* Featured */}
+      <div className="mt-6 tablet:mt-10 laptop:mt-15">
+        <div className="w-full p-2">
+          <h1 className="font-bold text-xs tablet:text-sm">Featured</h1>
+          <div
+            className={`flex-grow border-b ${
+              theme === "dark" ? "border-white" : "border-black"
+            } mt-2 laptop:mr-16`}
+          ></div>
+        </div>
+      </div>
 
-          {/* Einstein.ai */}
-          <div>
-            <WorkCard
-              key="Einstein"
-              img="/images/einstein.png"
-              name="Einstein.ai"
-              description="An all-in-one AI content generation website powered by OpenAI and ReplicateAI. Built with TypeScript, React, Next.js, Stripe, Crisp, Prisma, and MySQL."
-              onClick={() =>
-                window.open("https://github.com/aaronkjin/einstein")
-              }
-            />
-          </div>
+      <div className="mt-2 tablet:mt-4 laptop:mt-6 pb-8 grid grid-cols-1 tablet:grid-cols-2">
+        {/* Mentore */}
+        <div>
+          <WorkCard
+            key="Mentore"
+            img="/images/mentore.png"
+            name="Mentore"
+            description="An LLM-integrated mentor-matching platform. Built with JavaScript, React, HTML/CSS, OpenAI, Firebase, Flask, and Python's various ML libraries."
+            onClick={() => window.open("https://github.com/aaronkjin/mentore")}
+          />
+        </div>
 
-          {/* Quill */}
-          <div>
-            <WorkCard
-              key="Quill"
-              img="/images/star_wars.png"
-              name="Quill"
-              description="An ML algorithm for author attribution based on dialogue. Trained on Star Wars scripts. Built with Python and its various ML libraries."
-              onClick={() => window.open("https://github.com/aaronkjin/quill")}
-            />
-          </div>
+        {/* Einstein.ai */}
+        <div>
+          <WorkCard
+            key="Einstein"
+            img="/images/einstein.png"
+            name="Einstein.ai"
+            description="An all-in-one AI content generation website powered by OpenAI and ReplicateAI. Built with TypeScript, React, Next.js, Stripe, Crisp, Prisma, and MySQL."
+            onClick={() => window.open("https://github.com/aaronkjin/einstein")}
+          />
+        </div>
 
-          {/* Longevity */}
-          <div>
-            <WorkCard
-              key="Longevity"
-              img="/images/longevity.png"
-              name="Longevity"
-              description="A GPT system to suggest lifestyle habits for longer life, trained on custom ontology. Built with TypeScript, React, Next.js, Python, and its various ML libraries."
-              onClick={() =>
-                window.open("https://github.com/aaronkjin/longevity")
-              }
-            />
-          </div>
+        {/* Quill */}
+        <div>
+          <WorkCard
+            key="Quill"
+            img="/images/star_wars.png"
+            name="Quill"
+            description="An ML algorithm for author attribution based on dialogue. Trained on Star Wars scripts. Built with Python and its various ML libraries."
+            onClick={() => window.open("https://github.com/aaronkjin/quill")}
+          />
+        </div>
 
-          {/* Modify */}
-          <div>
-            <WorkCard
-              key="Modify"
-              img="/images/modify.png"
-              name="Modify"
-              description="A full-stack Spotify Clone website. Built with TypeScript, React, Next.js, Stripe, Supabase, and PostgreSQL."
-              onClick={() => window.open("https://github.com/aaronkjin/modify")}
-            />
-          </div>
+        {/* Longevity */}
+        <div>
+          <WorkCard
+            key="Longevity"
+            img="/images/longevity.png"
+            name="Longevity"
+            description="A GPT system to suggest lifestyle habits for longer life, trained on custom ontology. Built with TypeScript, React, Next.js, Python, and its various ML libraries."
+            onClick={() =>
+              window.open("https://github.com/aaronkjin/longevity")
+            }
+          />
+        </div>
 
-          {/* Bonsai */}
-          <div>
-            <WorkCard
-              key="Bonsai"
-              img="/images/bonsai.png"
-              name="Bonsai"
-              description="An iOS app for personal expense management. Built with Swift and SwiftUI."
-              onClick={() => window.open("https://github.com/aaronkjin/bonsai")}
-            />
-          </div>
+        {/* Modify */}
+        <div>
+          <WorkCard
+            key="Modify"
+            img="/images/modify.png"
+            name="Modify"
+            description="A full-stack Spotify Clone website. Built with TypeScript, React, Next.js, Stripe, Supabase, and PostgreSQL."
+            onClick={() => window.open("https://github.com/aaronkjin/modify")}
+          />
+        </div>
 
-          {/* Cass */}
-          <div>
-            <WorkCard
-              key="Cass"
-              img="/images/chatbot.png"
-              name="Cass"
-              description="An NLP chatbot that delivers personalized movie recommendations. Built with Python and its various ML libraries."
-              onClick={() =>
-                window.open(
-                  "https://gitfront.io/r/aaronkjin/3RJWBDccpb3M/chatbot-project/"
-                )
-              }
-            />
-          </div>
+        {/* Bonsai */}
+        <div>
+          <WorkCard
+            key="Bonsai"
+            img="/images/bonsai.png"
+            name="Bonsai"
+            description="An iOS app for personal expense management. Built with Swift and SwiftUI."
+            onClick={() => window.open("https://github.com/aaronkjin/bonsai")}
+          />
+        </div>
 
-          {/* Jonin */}
-          <div>
-            <WorkCard
-              key="Jonin"
-              img="/images/temp.png"
-              name="Jonin"
-              description="Currently building 👷‍♂️. A Python-based ninja platformer game. Built with Python, PyGame, and more."
-              onClick={() => window.open("https://github.com/aaronkjin/jonin")}
-            />
-          </div>
+        {/* Cass */}
+        <div>
+          <WorkCard
+            key="Cass"
+            img="/images/chatbot.png"
+            name="Cass"
+            description="An NLP chatbot that delivers personalized movie recommendations. Built with Python and its various ML libraries."
+            onClick={() =>
+              window.open(
+                "https://gitfront.io/r/aaronkjin/3RJWBDccpb3M/chatbot-project/"
+              )
+            }
+          />
+        </div>
+      </div>
 
-          {/* Physi.cs */}
-          <div>
-            <WorkCard
-              key="Physics"
-              img="/images/temp.png"
-              name="Physi.cs"
-              description="Currently building 👷‍♂️. A 2D physics engine built from scratch with no libraries. Built with C#."
-              onClick={() =>
-                window.open("https://github.com/aaronkjin/physics")
-              }
-            />
-          </div>
+      {/* Building */}
+      <div className="w-full p-2">
+        <h1 className="font-bold text-xs tablet:text-sm">Building</h1>
+        <div
+          className={`flex-grow border-b ${
+            theme === "dark" ? "border-white" : "border-black"
+          } mt-2 laptop:mr-16`}
+        ></div>
+      </div>
 
-          {/* Lumen */}
-          <div>
-            <WorkCard
-              key="Lumen"
-              img="/images/temp.png"
-              name="Lumen"
-              description="Currently building 👷‍♂️. Your go-to LMS platform. Built with TypeScript, React, Next.js, Stripe, Mux, Prisma, and PostgreSQL."
-              onClick={() => window.open("https://github.com/aaronkjin/lumen")}
-            />
-          </div>
+      <div className="mt-2 tablet:mt-4 laptop:mt-6 pb-16 grid grid-cols-1 tablet:grid-cols-2">
+        {/* Jonin */}
+        <div>
+          <WorkCard
+            key="Jonin"
+            img="/images/temp.png"
+            name="Jonin"
+            description="A Python-based ninja platformer game. Built with Python, PyGame, and more."
+            onClick={() => window.open("https://github.com/aaronkjin/jonin")}
+          />
+        </div>
+
+        {/* Physi.cs */}
+        <div>
+          <WorkCard
+            key="Physics"
+            img="/images/temp.png"
+            name="Physi.cs"
+            description="A 2D physics engine built from scratch with no libraries. Built with C#."
+            onClick={() => window.open("https://github.com/aaronkjin/physics")}
+          />
+        </div>
+
+        {/* Lumen */}
+        <div>
+          <WorkCard
+            key="Lumen"
+            img="/images/temp.png"
+            name="Lumen"
+            description="Your go-to LMS platform. Built with TypeScript, React, Next.js, Stripe, Mux, Prisma, and PostgreSQL."
+            onClick={() => window.open("https://github.com/aaronkjin/lumen")}
+          />
         </div>
       </div>
     </Layout>
