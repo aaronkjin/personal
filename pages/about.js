55650c696798259e5a8e2742f2dc9694eff82473
Aaron Jin <aaronkjin@gmail.com>
2024-05-29 11:13:30 -0700

    iphone

--- pages/about.js
+++ pages/about.js
@@ -95,7 +95,7 @@ export default function About() {
           <div className="w-full tablet:w-2/3 mt-1.5">
             <p className="text-sm tablet:text-base">
               Built team&apos;s first API gateway for an internal web app 
-              to test battery systems in Macs. Enhanced the web app with
+              used to test battery systems in iPhones. Enhanced the web app with
               ETL feature extraction.
             </p>
 
