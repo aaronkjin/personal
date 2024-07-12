db7ac72f140f45ec6424093c3b54982ce190fe00
aaronkjin <58490258+hoxykidmilli@users.noreply.github.com>
2024-07-11 20:24:08 -0700

    margin and padding alterations

--- components/Project/index.js
+++ components/Project/index.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 const Project = ({ img, name, description, onClick }) => {
   return (
-    <div className="rounded-lg p-2 first:ml-0 link laptop:w-[85%]">
+    <div className="rounded-lg p-2 first:ml-0 link laptop:w-[85%] pb-5 tablet:pb-7 laptop:pb-2">
       <div className="flex flex-col laptop:mt-5">
         <div
           className="relative rounded-lg transition-all ease-out duration-300 h-48 tablet:h-52 laptop:h-56 cursor-pointer overflow-hidden"
@@ -17,7 +17,7 @@ const Project = ({ img, name, description, onClick }) => {
             objectFit="cover"
           />
         </div>
-        <h1 className="mt-5 text-xl font-medium">
+        <h1 className="mt-3 laptop:mt-5 text-xl font-medium">
           {name ? name : "Project Name"}
         </h1>
         <h2 className="text-sm opacity-50">
