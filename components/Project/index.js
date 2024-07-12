079f782808a810c68ca2b3e34f1e3b69d1b2f619
aaronkjin <58490258+hoxykidmilli@users.noreply.github.com>
2024-07-11 20:09:07 -0700

    uniform project image sizes

--- components/Project/index.js
+++ components/Project/index.js
@@ -6,15 +6,15 @@ const Project = ({ img, name, description, onClick }) => {
     <div className="rounded-lg p-2 first:ml-0 link laptop:w-[85%]">
       <div className="flex flex-col laptop:mt-5">
         <div
-          className="relative rounded-lg transition-all ease-out duration-300 h-48 mob:h-auto cursor-pointer"
+          className="relative rounded-lg transition-all ease-out duration-300 h-48 cursor-pointer overflow-hidden"
           onClick={onClick}
         >
           <Image
             alt={name}
-            className="h-full w-full object-cover rounded-lg hover:scale-110 transition-all ease-out duration-300"
+            className="object-cover rounded-lg hover:scale-110 transition-all ease-out duration-300"
             src={img}
-            width={390}
-            height={200}
+            layout="fill"
+            objectFit="cover"
           />
         </div>
         <h1 className="mt-5 text-xl font-medium">
