1e8a00a60c1c9afc8bf5fff3636c1c1a8059194b
aaronkjin <58490258+hoxykidmilli@users.noreply.github.com>
2024-08-24 03:29:59 -0700

    intelligent find

--- pages/about.js
+++ pages/about.js
@@ -74,8 +74,8 @@ export default function About() {
           {/* Right Side */}
           <div className="w-full tablet:w-2/3 mt-1.5">
             <p className="text-sm tablet:text-base">
-              Integrating Apple Intelligence into Safari Suggestions for better
-              search relevancy. Building zero-keyword search for quick, easy
+              Integrated Apple Intelligence into Safari Suggestions to improve
+              search relevancy. Built Intelligent Find for non-text web
               searches.
             </p>
 
