0b0dbcda0806b047c2031433e0f64c72bc972566
aaronkjin <aaronkjin@gmail.com>
2024-12-06 01:37:06 -0800

    built by

--- components/FixedFooter/index.js
+++ components/FixedFooter/index.js
@@ -4,7 +4,7 @@ const FixedFooter = () => {
   return (
     <div className="fixed bottom-10 left-0 right-0 flex justify-center">
       <h1 className="text-xs text-bold p-2 laptop:p-0">
-        Built from scratch by Aaron Jin.
+        Built by Aaron Jin.
         {/* &#169; Aaron Jin. All rights reserved. */}
       </h1>
     </div>
--- components/Footer/index.js
+++ components/Footer/index.js
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <div className="flex justify-center">
       <h1 className="text-xs text-bold p-2 laptop:p-0">
-        Built from scratch by Aaron Jin.
+        Built by Aaron Jin.
         {/* &#169; Aaron Jin. All rights reserved. */}
       </h1>
     </div>
